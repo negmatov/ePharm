114a59b165270ffe96525fd6cc124024a999d7f8
negmatov <negmatov@list.ru>
2022-02-21 14:30:59 +0500

    # пишем свой валидатор поля в pydantic - урок 7

--- schemas.py
+++ schemas.py
@@ -1,10 +1,25 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import date
 
-#request body и pydantic модели - урок 3
-class work_hours(BaseModel):
+
+# request body и pydantic модели - урок 3
+class WorkHours(BaseModel):
     name: str
+
+
+# Body - урок 6
+class Owner(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+
+    # пишем свой валидатор поля в pydantic - урок 7
+    @validator('age')
+    def check_age(cls, v):
+        if v < 18:
+            raise ValueError('Возраст владельца должен быть больше 18 лет')
+        return v
 
 class Pharmacy(BaseModel):
     name_pharm: str
@@ -13,6 +28,6 @@ class Pharmacy(BaseModel):
     fax: str
     address: str
     way_mark: str
-    working_hours: List[work_hours]
+    working_hours: List[WorkHours]
     location: float
     refresh: date
